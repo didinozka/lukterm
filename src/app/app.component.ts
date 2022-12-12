ff2b18930a40938b54d59e96026e5162046181b5
dusan.drabik <dusan.drabik@touch4it.com>
2022-12-12 10:26:03 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Sanitizer, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'lukas-term';
+  public ipAddress: string = '';
+  public addresses: any[] = [];
+  height: number = 400;
+  width: number = 900;
+  isConfig = false;
+  password = 'LukasJePan';
+  clientName: string = '';
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.height = window.innerHeight - 112;
+    this.width = window.innerWidth / 2;
+  }
+
+  onSubmit() {
+    console.log(this.ipAddress)
+    // const sanitized = this.sanitizer.sanitize(SecurityContext.URL, this.ipAddress);
+    console.log('this.ipAddress', this.ipAddress);
+    
+    // const sanitized = this.ipAddress;
+    const sanitized = this.sanitizer.bypassSecurityTrustResourceUrl(this.ipAddress);
+    this.addresses.push(sanitized);
+    this.ipAddress = '';
+  }
+
+  onConfig() {
+    if (this.isConfig) {
+      this.isConfig = false;
+      return;
+    }
+    const pwd = prompt('Heslo:')
+    if (pwd === this.password) {
+      this.isConfig = true;
+    }
+  }
 }
